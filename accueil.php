70c644c0fac2a2fccdf868621e123c4bc1a3e891
Mamadou Dembele <Mdembelepro@gmail.com>
2021-08-24 16:23:38 +0200

    Finalisation des ajouts

--- accueil.php
+++ accueil.php
@@ -10,8 +10,10 @@ include('php/connexion.php');
     <link rel="icon" type="image/jpg" href="images/logo.jpg" />
     <link rel="stylesheet" href="css/accueil.css">
     <title>Yokke</title>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
     </script>
+
     <script>
         
       
@@ -40,12 +42,6 @@ $(document).ready(function()
         
         });
 });
-
-
-
-
-
-
         </script>
 </head>
 <body>
@@ -66,6 +62,7 @@ $(document).ready(function()
             </div>
             
         </header>
+
         
         <div id="container-banner">
             
@@ -76,43 +73,45 @@ $(document).ready(function()
         
         <div id="container-galerie">
             
-            <div class="box">
-                <button id="btn1"><a href="#premierformulaire">Curl debout</a></button>
-                <img src="images/curl_debout.jpg"  alt="">
-            </div>
+                    <div class="box">
+                        <button id="btn1"><a href="#premierformulaire">Curl debout</a></button>
+                        <img src="images/curl_debout.jpg"  alt="">
+                    </div>
+                    
+                    <div class="box">
+                        <button id="btn2"><a href="#secondformulaire">Développé couché</a></button>
+                        <img src="images/developpe_couché.jpg"  alt="">
+                    </div>
+                    
+                    <div class="box">
+                        <button id="btn3"><a href="#troisiemeformulaire">Développement des épaules</a></button>
+                        <img src="images/shoulders_development.jpg"  alt="">
+                    </div>
+                    
+                    <div class="box">
+                        <button id="btn4"><a href="#quatrièmeformulaire">Développement des triceps</a></button>
+                        <img src="images/triceps_development.jpg"  alt="">
+                    </div>
             
-            <div class="box">
-                <button id="btn2"><a href="#secondformulaire">Développé couché</a></button>
-                <img src="images/developpe_couché.jpg"  alt="">
-            </div>
-            
-            <div class="box">
-                <button id="btn3"><a href="#troisiemeformulaire">Développement des épaules</a></button>
-                <img src="images/shoulders_development.jpg"  alt="">
-            </div>
-            
-            <div class="box">
-                <button id="btn4"><a href="#quatrièmeformulaire">Développement des triceps</a></button>
-                <img src="images/triceps_development.jpg"  alt="">
-            </div>
-            
-        </div>
-       
+                </div>
+                
+                <!-- Formulaire -->
         <?php
-        include('formulaires/formulaire_curl_debout.php');
-        include('formulaires/formulaire_développé_couché.php');
-        include('formulaires/formulaire_développement_épaules.php');
-        include('formulaires/formulaire_développement_des_triceps.php');
+        
+                include('formulaires/formulaire_curl_debout.php');
+                include('formulaires/formulaire_développé_couché.php');
+                include('formulaires/formulaire_développement_épaules.php');
+                include('formulaires/formulaire_développement_des_triceps.php');
         
         ?>
 
         <div id="footer">
-            <footer>
-                <p>©2021 Yokke,INC. Tous droits réservés</p>
-                
-            </footer>
+                    
+                    <footer>
+                        <p>©2021 Yokke,INC. Tous droits réservés</p>
+                        
+                    </footer>
         </div>
-        <!-- Formulaire -->
 
     </div>
         
